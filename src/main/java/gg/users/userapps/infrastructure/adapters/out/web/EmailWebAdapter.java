ca62fd53532a3f670467839ff676f0e11d5fdf27
renz.ayala <renzayala98@gmail.com>
2026-08-25 00:25:14 -0500

    feat: email fixs, ready to try.

--- src/main/java/gg/users/userapps/infrastructure/adapters/out/web/EmailWebAdapter.java
+++ src/main/java/gg/users/userapps/infrastructure/adapters/out/web/EmailWebAdapter.java
@@ -5,23 +5,32 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailWebAdapter implements EmailWebPort {
     private final JavaMailSender javaMailSender;
+    private final RestClient restClient;
 
     @Value("${java.mail.from}")
     private String from;
 
-    @Override
+    @Value("${spring.mail.password}")
+    private String apiKey;
+
+
     @Async
-    public void sendEmailConfirmation(String to, String text, String subject) {
+    public void sendEmailConfirmationWithDomain(String to, String text, String subject) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
 
@@ -32,6 +41,31 @@ public class EmailWebAdapter implements EmailWebPort {
             helper.setText(text, true);
 
             javaMailSender.send(message);
+            log.info("email sent successfully to {}", to);
+        } catch (Exception e) {
+            log.error("Error sending email to {}", to, e);
+        }
+    }
+
+    @Override
+    @Async
+    public void sendEmailConfirmation(String to, String text, String subject) {
+        try {
+            var body = Map.of(
+                    "sender", Map.of("name", "HexaAuth", "email", from),
+                    "to", List.of(Map.of("email", to)),
+                    "subject", subject,
+                    "htmlContent", text
+            );
+
+            restClient.post()
+                    .uri("https://api.brevo.com/v3/smtp/email")
+                    .header("api-key", apiKey)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(body)
+                    .retrieve()
+                    .toBodilessEntity();
+
             log.info("email sent successfully to {}", to);
         } catch (Exception e) {
             log.error("Error sending email to {}", to, e);
